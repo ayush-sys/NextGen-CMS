a3841392e1939bc3049f35e23dea15d67e05b866
Ayush Pattanayak <ayushmp7@gmail.com>
2022-10-10 20:25:32 +0530

    navbar v1.2 [completed]

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,29 @@
 import '../styles/globals.css'
+import { createTheme, ThemeProvider } from '@mui/material'
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#256D85'
+    },
+    secondary: {
+      main: '#DFF6FF'
+    }
+  },
+  typography: {
+    fontFamily: 'Montserrat',
+    h6: {
+      fontFamily: 'Ubuntu'
+    }
+  }
+});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
